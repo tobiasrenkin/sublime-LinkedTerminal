cac933b0935141d5243547b00f4bf5039c6bd7b2
Tobias Renkin <tobias.renkin@gmail.com>
2019-11-06 20:14:10 +0100

    initial commit

--- console.py
+++ console.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import os, sys, re, readline, select, signal, termios, tty, pty, threading, time, subprocess, atexit, argparse
+import os, sys, select, signal, termios, tty, pty, subprocess, atexit, argparse
 
 parser = argparse.ArgumentParser(description='Sublime Console connects your terminal to Sublime Text through a named pipe.')
-parser.add_argument('--pipe_exec', default="/tmp/sublime_exec.fifo", action="store", dest="pipe_exec", help="Create pipe at this location.")
-parser.add_argument('--pipe_print', default="/tmp/sublime_print.fifo", action="store", dest="pipe_print", help="Create pipe at this location.")
+parser.add_argument('--pipe_exec', default="/tmp/sublime_console.fifo", action="store", dest="pipe_exec", help="Create pipe at this location.")
 parser.add_argument('--shell', default="bash", action="store", dest="shell", choices=["bash", "sh", "zsh", "fish"], help="Shell to run.")
 args = parser.parse_args()
 shell_cmd = args.shell
 fifo_exec_loc = args.pipe_exec
-fifo_print_loc = args.pipe_print
 
 print('Sublime Console v0.1.')
 
@@ -36,14 +34,11 @@ else:
 	os.mkfifo(fifo_exec_loc, 0777)
 	print("Executing from "+fifo_exec_loc)
 
-# save original tty setting then set it to raw or cbreak mode; that means everything will be passed to pty immediately
-# as it is typed and parent terminal will not translate ctrl+c etc. into signals.
+# save original tty setting then set it to raw or cbreak mode
 old_tty = termios.tcgetattr(sys.stdin)
 tty.setraw(sys.stdin.fileno())
 
-# open pty and bash subprocess; connect all pipes of bash to slave half of pty.
-# writing to master_fd is input to slave_fd and hence bash.
-# output of bash is output in slave_fd and hence is an input in master_fd
+# open pty and bash subprocess; connect all pipes of bash subprocess to slave half of pty.
 master_fd, slave_fd = pty.openpty()
 p = subprocess.Popen(shell_cmd,
 	preexec_fn=os.setsid,
@@ -52,7 +47,7 @@ p = subprocess.Popen(shell_cmd,
 	stderr=slave_fd,
 	universal_newlines=True)
 
-# connect tty I/O to pty
+# open pipe, forward stdin/fifo to pty master, write master to stdout
 fifo_exec = os.open(fifo_exec_loc, os.O_RDWR)
 while p.poll() is None:
 	try:
