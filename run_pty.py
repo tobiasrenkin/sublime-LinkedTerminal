fcd5a9bb93e0147aa65e905102bced5b5a111bbe
Tobias Renkin <tobias.renkin@gmail.com>
2019-11-07 13:30:37 +0100

    Small bugfixes

--- run_pty.py
+++ run_pty.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import os, sys, select, signal, termios, tty, pty, subprocess, atexit, argparse
+import os, sys, select, signal, termios, fcntl, tty, pty, subprocess, atexit, argparse, struct, time
 
 parser = argparse.ArgumentParser(description='Sublime Console connects your terminal to Sublime Text through a named pipe.')
 parser.add_argument('--pipe_exec', default="/tmp/sublime_console.fifo", action="store", dest="pipe_exec", help="Create pipe at this location.")
@@ -18,7 +18,7 @@ def delete_fifo():
 
 # set event handler
 def sigintHandler(signum, frame):
-	os.close(fifo_exec)
+	os.close(fifo_fd)
 	delete_fifo()
 	termios.tcsetattr(sys.stdin.fileno(), termios.TCSADRAIN, old_tty)
 	sys.exit("Closing Sublime Console.")
@@ -26,6 +26,20 @@ signal.signal(signal.SIGINT, sigintHandler)
 signal.signal(signal.SIGHUP, sigintHandler)
 signal.signal(signal.SIGTERM, sigintHandler)
 atexit.register(delete_fifo)
+
+# make sure parent terminal and pty master have same dimensions; always resize on start
+mstr_h = 0
+mstr_w = 0
+def ensure_equal_size():
+	global mstr_h, mstr_w
+	prnt_h, prnt_w, hp, wp = struct.unpack('HHHH', fcntl.ioctl(0, termios.TIOCGWINSZ, struct.pack('HHHH', 0, 0, 0, 0)))
+	if prnt_h!=mstr_h or prnt_w!=mstr_w:
+		tiocswinsz = getattr(termios, 'TIOCSWINSZ', -2146929561)
+		size_update = struct.pack('HHHH', prnt_h, prnt_w, 0, 0)
+		fcntl.ioctl(master_fd, tiocswinsz, size_update)
+    	mstr_h=prnt_h
+    	mstr_w=prnt_w
+
 
 # create fifo;
 if os.path.exists(fifo_exec_loc):
@@ -48,22 +62,32 @@ p = subprocess.Popen(shell_cmd,
 	universal_newlines=True)
 
 # open pipe, forward stdin/fifo to pty master, write master to stdout
-fifo_exec = os.open(fifo_exec_loc, os.O_RDWR)
+fifo_fd = os.open(fifo_exec_loc, os.O_RDWR)
+last_resize_check = 0
+
 while p.poll() is None:
 	try:
-		r, w, e = select.select([sys.stdin, master_fd, fifo_exec], [], [])
-		if sys.stdin in r:
-			stdinput = os.read(sys.stdin.fileno(), 10240)
-			if stdinput:
-				os.write(master_fd, stdinput)
-		elif fifo_exec in r:
-			fifoinput = os.read(fifo_exec, 10240)
-			if fifoinput:
-				os.write(master_fd, fifoinput)
+		r, w, e = select.select([sys.stdin, master_fd, fifo_fd], [], [])
+		
+		# check for terminal resize when input or output is received; at most once every second
+		if time.time()-last_resize_check>1:
+			ensure_equal_size()
+			last_resize_check = time.time()
+
+		if sys.stdin in r or fifo_fd in r:
+			if sys.stdin in r:
+				pty_input = os.read(sys.stdin.fileno(), 10240)
+			elif fifo_fd in r:
+				pty_input = os.read(fifo_fd, 10240)
+			
+			if pty_input:
+				os.write(master_fd, pty_input)
+
 		elif master_fd in r:
 			o = os.read(master_fd, 10240)
 			if o:
 				os.write(sys.stdout.fileno(), o)
+
 	except select.error as ex:
 		if ex[0] == 4:
 			pass
--- sublime-console.py
+++ sublime-console.py
@@ -2,15 +2,27 @@ import sublime, sublime_plugin
 import os, sys
 from subprocess import Popen
 
-
 class LaunchSublimeConsoleCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		settings = sublime.load_settings("sublime-console.sublime-settings")
+		if settings.get("launch_in_cwd"):
+			launch_in=self.window.extract_variables()["file_path"]
+		else:
+			launch_in = "~"
 
-		python_cmd = "python "+sublime.packages_path()+"/sublime-console/console.py --pipe_exec "+settings.get("pipe_exec_location")+" --shell "+settings.get("shell")
-		launch_cmd = " ".join([settings.get("terminal"),'--title="Sublime Console"',"-x",settings.get("shell"),"-c",'"cd ~; ',python_cmd,'"'])
+		python_cmd = " ".join([
+			"python", sublime.packages_path()+"/sublime-console/run_pty.py",
+			"--pipe_exec", settings.get("pipe_exec_location"),
+			"--shell", settings.get("shell")
+			])
+		launch_cmd = " ".join([
+			settings.get("terminal"),'--title="Sublime Console"',"-x",
+			settings.get("shell"),"-c",
+			'"cd '+launch_in+'; '+python_cmd+'"'
+			])
+
 		if os.path.exists(settings.get("pipe_exec_location")) and not(settings.get("allow_multiple_inst")):
-			print("Warning: "+settings.get("pipe_exec_location")+" already exists. There is probably already a Sublime Console running and I won't open a new one.")
+			print("Warning:", settings.get("pipe_exec_location"), "already exists. There is probably already a Sublime Console running and I won't open a new one.")
 		else:
 			p = Popen(launch_cmd, shell=True)
 
@@ -26,8 +38,7 @@ class SendCommand(sublime_plugin.TextCommand):
 		
 		try:
 			with open(settings.get("pipe_exec_location"), "w") as fifo:
-				writestring = "\n".join(code) + "\n"
-				fifo.write(writestring)
+				fifo.write("\n".join(code) + "\n")
 		except:
-			print("Error: Can't write to pipe at "+settings.get("pipe_exec_location"))
+			print("Error: Can't write to pipe at", settings.get("pipe_exec_location"))
 
