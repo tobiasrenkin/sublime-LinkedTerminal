ce9bd3d4f3444e7743ff9eb4d4dd46f060c313b2
Tobias Renkin <tobias.renkin@gmail.com>
2020-05-22 16:08:01 +0200

    v2

--- readme.md
+++ readme.md
@@ -1,13 +1,13 @@
-## Sublime Console
+## Sublime Console v0.1.
 
 A plugin for Sublime Text on Linux that connects an external terminal to Sublime Text.
 
 ### What does the plugin do?
 
-Sublime Console is a Sublime Text plugin that opens a terminal. The terminal is connected to a named pipe. It can receive input from both the system stdin and the pipe. Sublime Text (or any other application) can communicate with the terminal through the pipe.
+Sublime Console is a Sublime Text plugin that opens a terminal. The terminal is connected to a named pipe and can receive input from both the system stdin and the pipe. Sublime Text (or any other application) can write to the pipe to run build commands etc.
 
 ### Why is this useful?
-* The plugin comes with a built-in "send" command. The send command sends text selected in a Sublime Text view to the terminal and the text is then interpreted by the shell (or whatever other program is open in the terminal).
+* The plugin comes with a "sublime_console_send" command. This command sends text selected in a Sublime Text view to the terminal, and the text is then interpreted by the shell (or whatever other program is open in the terminal). 
 * You can write build systems that send build commands directly to the pipe. The build command then runs in the external terminal instead of within Sublime. This is useful for interactive programs, debugging, or if you just like your external terminal.
 
 ### Settings
--- sublime-console.py
+++ sublime-console.py
@@ -1,44 +1,71 @@
 import sublime, sublime_plugin
 import os, sys
-from subprocess import Popen, check_output, STDOUT
+import subprocess
 from random import randrange
 
-class LaunchSublimeConsoleCommand(sublime_plugin.WindowCommand):
-	def run(self):
-		global pipe_location
-		global pipe_no
-		
-		isrunning = int(check_output(['wmctrl -l | grep "Sublime Console" | wc -l'], shell=True).strip())
-		if isrunning > 0:
-			print("Sublime Console is already open.")
-			Popen('wmctrl -a "Sublime Console"', shell=True)
+console = None
+
+class SublimeConsole():
+	def __init__(self, settings):
+		self.pipe_no = randrange(100000)
+		self.pipe_location = "/tmp/sublime_console_" + str(self.pipe_no)
+		self.p = None
+		self.settings = settings
+
+	def send(self, cmd):
+		try:
+			with open(self.pipe_location, "w") as fifo:
+				fifo.write(cmd + "\n")
+		except:
+			print("Error: Can't write to pipe at", self.pipe_location)
+
+	def isalive(self):
+		#open_consoles = int(check_output(['wmctrl -l | grep "Sublime Console {0}" | wc -l'.format(self.pipe_no)], shell=True).strip())
+		if self.p == None:
+			return False
 		else:
-			pipe_no = str(randrange(100000))
-			pipe_location = "/tmp/sublime_console_"+pipe_no
-		
-			settings = sublime.load_settings("sublime-console.sublime-settings")
-			if settings.get("launch_in_cwd"):
-				launch_in=self.window.extract_variables()["file_path"]
+			response = self.p.poll()
+			print("response", response)
+			if response == None:
+				return True
+			else:
+				return False
+
+	def launch(self):
+		if self.isalive():
+			subprocess.Popen('wmctrl -a "Sublime Console {0}"'.format(self.pipe_no), shell=True)
+			print("Console is alive")
+		else:
+			if self.settings.get("launch_in_cwd"):
+				#launch_in=self.window.extract_variables()["file_path"]
+				launch_in = "~"
 			else:
 				launch_in = "~"
-
 			python_cmd = " ".join([
 				"python", sublime.packages_path()+"/sublime-console/run_pty.py",
-				"--pipe", pipe_location,
-				"--shell", settings.get("shell"),
-				"--raise_on_input" if settings.get("raise_on_input")==True else ""
-				])
+				"--pipe", self.pipe_location,
+				"--shell", self.settings.get("shell"),
+				"--raise_on_input" if self.settings.get("raise_on_input")==True else ""
+			])
 			launch_cmd = " ".join([
-				settings.get("terminal"),'--title="Sublime Console {0}"'.format(pipe_no),"-x",
-				settings.get("shell"),"-c",
+				self.settings.get("terminal"),'--title="Sublime Console {0}"'.format(self.pipe_no),"-x",
+				self.settings.get("shell"),"-c",
 				'"cd '+launch_in+'; '+python_cmd+'"'
-				])
+			])
+			self.p = subprocess.Popen(launch_cmd, shell=True)
+			print(self.p.poll())
 
-			p = Popen(launch_cmd, shell=True)
+class SublimeConsoleLaunchCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		global console
+		if not(console):
+			settings = sublime.load_settings("sublime-console.sublime-settings")
+			console = SublimeConsole(settings)
+		console.launch()
 
-class SendCommand(sublime_plugin.TextCommand):
+class SublimeConsoleSendCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		settings = sublime.load_settings("sublime-console.sublime-settings")
+		global console
 
 		self.view.run_command("expand_selection", {"to": "line"})
 		selection = self.view.sel()
@@ -46,21 +73,11 @@ class SendCommand(sublime_plugin.TextCommand):
 		for region in selection:
 			code.append(self.view.substr(region).rstrip())
 		
-		try:
-			with open(pipe_location, "w") as fifo:
-				fifo.write("\n".join(code) + "\n")
-		except:
-			print("Error: Can't write to pipe at", settings.get("pipe_exec_location"))
-
+		console.send("\n".join(code) + "\n")
+		
 class SublimeConsoleExec(sublime_plugin.WindowCommand):
 	def run(self, cmd):
-		settings = sublime.load_settings("sublime-console.sublime-settings")
-		print(cmd)
-		try:
-			#settings.get("pipe_exec_location")
-			with open(pipe_location, "w") as fifo:
-				fifo.write(cmd + "\n")
-		except:
-			print("Error: Can't write to pipe at", settings.get("pipe_exec_location"))
-
+		global console
+		console.send(cmd)
+		
 
